corrects capacitance calculation

capacitance.py
@@ -16,6 +16,11 @@ print('C = ', C)
 e = 1.602e-19
 h = 6.626e-34
 hbar = h / (2 * np.pi)
+Fq = h/(2*e)
+Ec_Ej_ratio = 50
+deltaAl = 190e-6*1.602176634e-19 #eV
+
+print('delta Al = ', deltaAl)
 
 # M = np.array([[1, 1, 0], [1, -1, 0], [0, 0, 1]])
 M = np.array([[1, 1], [1, -1]])
@@ -28,11 +33,20 @@ print('Kinv[1,1] = ', Kinv[1,1])
 Ec = e**2/2*Kinv[1,1]
 print('Ec (Joules) = ', Ec)
 
-Ec = 0.350#Ec/h*10**-9
-print('Ec (GHz) = ', Ec)
+Ec_f = Ec/h*10**-9 #0.350#
+print('Ec (GHz) = ', Ec_f)
 
-Ej = Ec*50
-print('Ej (GHz) = ', Ej)
+Ej = Ec*Ec_Ej_ratio
+print('Ej (Joules) = ', Ej)
 
-omage = np.sqrt(8.0*Ec*Ej)
-print('omage (GHz) = ', omage)
+Ej_f = Ec_f*Ec_Ej_ratio
+print('Ej (GHz) = ', Ej_f)
+
+omaga = np.sqrt(8.0*Ec_f*Ej_f)-Ec_f
+print('omaga (GHz) = ', omaga)
+
+Ic = Ej*2*np.pi/Fq
+print('Ic (A) = ', Ic)
+
+Rj = np.pi*deltaAl/(Ic*2*e)
+print('Rj (Ohm) = ', Rj)